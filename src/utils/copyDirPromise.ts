eaf296ed48081d36c25000db5d0d9847ed3388aa
Divlo <contact@divlo.fr>
2020-05-22 03:00:26 +0200

    👌 IMPROVE: .npmignore to .gitignore

--- src/utils/copyDirPromise.ts
+++ src/utils/copyDirPromise.ts
@@ -1,11 +1,24 @@
-import { ncp } from "ncp";
+import fs from 'fs';
+import path from 'path';
 
 function copyDirPromise(source: string, destination: string) {
     return new Promise((next) => {
-        ncp(source, destination, (error) => {
-            if (error) return next(error);
-            next();
+        const filesToCreate = fs.readdirSync(source);
+        filesToCreate.forEach(async (file) => {
+            const originalFilePath = path.join(source, file);
+            const stats = fs.statSync(originalFilePath);
+
+            if (stats.isFile()) {
+                const contents = fs.readFileSync(originalFilePath, "utf8");
+                if (file === '.npmignore') file = '.gitignore';
+                const writePath = path.join(destination, file);
+                fs.writeFileSync(writePath, contents, "utf8");
+            } else if (stats.isDirectory()) {
+                fs.mkdirSync(path.join(destination, file));
+                await copyDirPromise(path.join(source, file), path.join(destination, file));
+            }
         });
+        next();
     });
 }
 
