41f2be583372aed6bc833f4ba019a1168ab1ad69
divlo <contact@divlo.fr>
2021-04-19 15:08:49 +0200

    fix(templates): update fastify add HOST variable

--- templates/api/fastify/template/src/index.ts
+++ templates/api/fastify/template/src/index.ts
@@ -2,10 +2,11 @@ import { application } from './application'
 import { sequelize } from './tools/database/sequelize'
 
 const PORT = parseInt(process.env.PORT ?? '8080', 10)
+const HOST = process.env.HOST ?? '0.0.0.0'
 
 const main = async (): Promise<void> => {
   await sequelize.sync()
-  const address = await application.listen(PORT, process.env.HOST)
+  const address = await application.listen(PORT, HOST)
   console.log('\x1b[36m%s\x1b[0m', `🚀  Server listening at ${address}`)
 }
 
